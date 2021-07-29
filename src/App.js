3f4b7e384d247852742bb89800ac0a8a555dc1e4
cezar04 <iancu.m.cezar@gmail.com>
2021-07-29 14:32:57 +0300

    category component draft, full responsive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Categories from "./components/Categories";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Header/>
+      <Categories/>
     
     </div>
   );
