46c64fc84345030bd594c6afc42bf3e0f6fe58a8
cezar04 <iancu.m.cezar@gmail.com>
2021-08-18 11:27:34 +0300

    working filtering by category

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,10 +4,16 @@ import ProductCard from './ProductCard'
 import {Link} from 'react-router-dom'
 import data from "../data/data";
 
+import {useState} from 'react';
+
 // trebuie sa iei id ul produsul sa il dai
 
 function Products() {
-const {products}= data
+// dupa ce faci asta, sa te gandesti la cum pui totul in cartContext, si daca te ajuta
+
+  const [products, setProducts] = useState(data.products)
+  const [productCopy, setProductCopy] = useState(products)
+
 const lista = []
 
 for(let element of products.map((item)=>item.categoryType)){
@@ -20,16 +26,24 @@ const removeDublicate =(lista)=>{
 
 const handleClick= (e)=>{
   console.log(e.target.value)
+  let productCopy;
+  if(e.target.value==="all"){
+    setProductCopy(products)
+  }
+  else{
+    let filteredData = products.filter(item=>item.categoryType===e.target.value)
+    setProductCopy(filteredData)
+  }
 }
 
 
 
     return (
-        <div className="bg-gray-200">
+        <div className="bg-gray-200 pb-20">
            <div className="pt-20  border text-center  bg-white flex justify-center">
-             <FilterMeniu item={"all"} handleClick={handleClick} />
+              <FilterMeniu item={"all"} handleClick={handleClick} />
             {removeDublicate(lista).map((item)=>(
-            <FilterMeniu item={item} handleClick={handleClick}  />
+              <FilterMeniu item={item} handleClick={handleClick}  />
             ))}
                 
          
@@ -38,7 +52,7 @@ const handleClick= (e)=>{
            <div className="grid sm:grid-cols-4 gap-3 sm:pl-24 sm:pr-24 p-3  ">
             
            {
-             products.map((item)=>(
+             productCopy.map((item)=>(
              
               <div  key={item.id} id="RouterNavLink"><ProductCard  item={item}/></div>
              
