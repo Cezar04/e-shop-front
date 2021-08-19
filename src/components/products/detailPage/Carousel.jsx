f683f23997aba2b302a970686ca62fd861fac165
cezar04 <iancu.m.cezar@gmail.com>
2021-08-20 01:03:27 +0300

    detail page

---
+++ src/components/products/detailPage/Carousel.jsx
@@ -0,0 +1,63 @@
+import { Carousel } from 'react-carousel-minimal';
+
+
+const captionStyle = {
+  fontSize: '2em',
+  fontWeight: 'bold',
+}
+const slideNumberStyle = {
+  fontSize: '20px',
+  fontWeight: 'bold',
+}
+
+const Carouselul = (images, name) => {
+  const data = [
+    {
+      image: images.images.pic1,
+      caption:""
+    },
+    {
+      image: images.images.pic2,
+      caption: ""
+    },
+    {
+      image: images.images.pic3,
+      caption: ""
+    }
+    
+  ];
+  
+  console.log(images.images.pic1)
+  return(
+<div className="App pt-20">
+      <div style={{ textAlign: "center" }}>
+        
+        <div className="" style={{
+          
+        }}>
+          <Carousel
+            data={data}
+            time={4000}
+            width="200rem"
+            height="500px"
+            captionStyle={captionStyle}
+            radius="10px"
+            slideNumber={true}
+            slideNumberStyle={slideNumberStyle}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            thumbnails={true}
+            thumbnailWidth="100px"
+        
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Carouselul;
--- src/components/products/detailPage/ProductPage.jsx
+++ src/components/products/detailPage/ProductPage.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
-import Carousel from 'react-elastic-carousel';
 import {Link} from 'react-router-dom';
+import data from '../../data/data'
+import Carouselul from './Carousel';
+import CartContext from '../../../context/cart/CartContext';
+import {useContext} from 'react';
 
 
-function ProductPage() {
+
+function ProductPage(props) {
+    const {addToCart} = useContext(CartContext)
+
+    const productId=props.match.params.id;
+    const datatata=data.products
+    const renderData=datatata[productId-1]
+
+
+    const images=renderData.images
+    const name= renderData.name
 
     return (
-        <div className="sm:flex">
-            <Carousel className="pt-36 sm:pb-36" >
+        <>
 
-                <div className=" sm:m-9  w-screen sm:h-96  ">
-                    <img className="" src="https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-                    <h1 className="bg-yellow-500 p-3 text-white sm:text-4xl font-medium uppercase">Orange Sofa</h1>
 
-                </div>
-                <div className=" sm:m-9  w-screen sm:h-96 ">
-                    <img className="" src="https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-                    <h1 className="bg-yellow-500 p-3 text-white sm:text-4xl font-medium uppercase">Orange Sofa</h1>
+        <div className="sm:flex pb-36 sm:p-20 sm:pb-36">
 
-                </div>
-                <div className=" sm:m-9  w-screen sm:h-96 ">
-                    <img className="" src="https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
-                    <h1 className="bg-yellow-500 p-3 text-white sm:text-4xl font-medium uppercase">Orange Sofa</h1>
+                <Carouselul images={images} name={name}/>
 
-                </div>
-                
-
-            </Carousel>
             <div className="sm:pt-36 p-10 container">
-               
-                <h1 className="font-bold text-5xl sm:pt-8">Numele canapelei</h1>
-                <h1 className="text-lg pt-5  container text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, vero dolorem cupiditate amet neque voluptatibus voluptatum commodi deleniti laudantium sequi, optio eius sunt dolores natus eos dignissimos velit accusantium! Dicta.</h1>
-                <h1 className="sm:pt-20 pt-10 text-3xl font-bold">3000 <span>lei</span></h1>
+       
+                <h1 className="font-bold text-5xl sm:pt-8">{renderData.name}</h1>
+                <h1 className="text-lg pt-5  container text-gray-500">{renderData.discription}</h1>
+                <h1 className="sm:pt-20 pt-10 text-2xl font-bold">Price {renderData.price} <span>$</span></h1>
                 <div className="pt-20 pb-10">
-                <Link to="/shopping-cart" className=" bg-yellow-500 rounded hover:shadow-xl items-center p-4 font-medium text-white hover:text-black" >Add to Shopping Cart <i className="fas fa-shopping-cart text-red-500 "></i></Link>
+                <div onClick={()=>addToCart(renderData)} className="cursor-pointer bg-yellow-500 rounded hover:shadow-xl items-center p-4 sm:w-1/4 font-medium text-white hover:text-black" >Add to Shopping Cart <i className="fas fa-shopping-cart text-red-500 "></i></div>
+            
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
